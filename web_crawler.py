94bff1d15a85b0a9d4a862e85d930213d3cb4ce4
Joao <joaolucas0810@gmail.com>
2022-08-19 21:29:40 -0300

    first commit

---
+++ web_crawler.py
@@ -0,0 +1,45 @@
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import  webdriver
+from time import sleep
+
+navegador = webdriver.Chrome()
+
+navegador.get('https://www.tripadvisor.com.br')
+sleep(2)
+
+#localizar a barra de pesquisa
+input_place = navegador.find_element('input')
+sleep(1)
+
+#digitar as palavras chaves 
+input_place.send_keys('Congresso Nacional - Brasília')
+sleep(1)    
+
+#encontrar a opção que tenha como botão uma foto e este será o único resultado encontrado com estas palavras chaves.
+button_stay = navegador.find_element('button > picture')
+sleep(1)
+button_stay.click()
+
+#após entrar na pagina do congresso nacional, precisamos localizar o botão que redirecionará para a média e quantidade de avaliações.
+Searchsvg= navegador.find_element('svg')
+Searchsvg.click()
+
+page.content = navegador. page_source
+site = BeautifulSoup(page.content, 'html.parser')
+
+#encontrar o valor da média e quantidade de avaliações atráves da localização especifica dos atributos.
+media = site.find('div', attrs={'class': 'f u j'}) 
+
+avaliacoes = site.find('div', attrs={'span': 'class'}) 
+print(avaliacoes.prettify())
+
+print('## Resultado da coleta de dados ##')
+print('Avaliação do local: ', avaliacoes, 'de ', media)
+
+
+
+
+
+
